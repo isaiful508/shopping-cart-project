66f54a5aa165a528869eef4ceec305b208bcd04e
isaiful508 <isaiful.islam508@gmail.com>
2024-03-15 12:14:48 +0600

    almost done project without good looking

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,13 @@ import SingleProduct from './SingleProduct';
 
 function App() {
 
+  //product state
   const [products, setProducts] = useState([]);
+  //cart state
+  const [cart, setCart] = useState([]);
 
-    useEffect(() => {
+  //ueseEffect for fetch data
+  useEffect(() => {
     fetch("./fakeData.json")
       .then(res => res.json())
       .then(data => {
@@ -16,15 +20,37 @@ function App() {
       })
   }, [])
 
-  console.log(products);
 
+  //cart btn handler
+  const handleBtnCart = (p) => {
+    const isExists = cart.find(pd => pd.id == p.id);
+    if (!isExists) {
+      setCart([...cart, p]);
+    }
+    else {
+      alert("Already added")
+    }
+  }
+
+//remove btn handler
+const handleRemoveBtn = (id) => {
+  const newCart = cart.filter(item => item.id !== id)
+  setCart(newCart);
+}
+
+
+
+
+
+
+  // console.log(cart);
   return (
     <>
       <div className="main-container">
 
         <div className="cards-container">
           {
-            products.map((pd) => <SingleProduct product={pd} key={pd.id}></SingleProduct>)
+            products.map((pd) => <SingleProduct handleBtnCart={handleBtnCart} product={pd} key={pd.id}></SingleProduct>)
           }
 
           <div className="card">
@@ -33,7 +59,25 @@ function App() {
         </div>
 
         <div className="cart-container">
-          <h1>This is card</h1>
+          <div className="card-title">
+            <h5>Name:</h5>
+            <h5>Price:</h5>
+          </div>
+
+          <div>
+            {
+              cart.map((item, index) => (
+
+                <div key={item.id}  className="cart-info">
+                  <p>{index + 1}</p>
+                  <h5>{item.title.slice(0,10)}</h5>
+                  <h5>{item.price}</h5>
+                  <button  onClick={()=> handleRemoveBtn(item.id)}>Remove</button>
+                </div>
+
+              ))
+            }
+          </div>
         </div>
 
       </div>
--- src/SingleProduct.jsx
+++ src/SingleProduct.jsx
@@ -1,19 +1,20 @@
 
 
-const SingleProduct = ({product}) => {
-    console.log(product);
+const SingleProduct = ({product, handleBtnCart}) => {
+    // console.log(handleBtnCart);
+   const {title, price, description, image, id}  = product
     return (
         <div>
             
           <div className="card">
-            <img className='card-img' src="https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg" alt="" />
-            <h1>Tesr Test</h1>
+            <img className='card-img' src={image} alt="" />
+            <h1>{title.slice(0, 10)}</h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, quisquam vero. Eaque quae magnam illum.
+             {description}
             </p>
             <div className="card-footer">
-              <h1>520 $</h1>
-              <button className='add-btn'>Add To Cart</button>
+              <h1>{price}</h1>
+              <button onClick={(e)=> handleBtnCart(product)} className='add-btn'>Add To Cart</button>
             </div>
           </div>
         </div>
